e7f25c731edb01a816496992ca9485949487a330
Ivysonin <ivysonthauan4@gmail.com>
2025-09-22 20:43:45 -0300

    somando um número int com número float

---
+++ Lógica_de_programação/variaveis2.py
@@ -0,0 +1,7 @@
+# Entrada de dados 
+num1 = int(input('Digite um número inteiro(n): ')) # Entrando um valor inteiro
+num2 = float(input('Digite um número decimal(n.n): ')) # Entrando um valor float
+soma = num1 + num2 # Somando os valores
+
+# Exibindo
+print(f'{num1} + {num2} = {soma}')
