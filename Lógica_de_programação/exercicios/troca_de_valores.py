7060b263207241aa8a4562060d15a6235ffaa570
Ivysonin <ivysonthauan4@gmail.com>
2025-09-29 17:44:18 -0300

    Fazendo uma troca de valores

---
+++ Lógica_de_programação/exercicios/troca_de_valores.py
@@ -0,0 +1,12 @@
+a = 'Python'
+b = 'Java'
+
+print(f'O valor de A: {a}')
+print(f'O valor de B: {b}')
+
+print('\nTROCA DE VALORES\n')
+
+a,b  = b,a
+
+print(f'O valor de A: {a}')
+print(f'O valor de B: {b}')
