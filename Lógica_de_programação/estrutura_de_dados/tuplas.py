fbb26a641f964b1e97f7f98f4dbe1cf2ce07a7f3
Ivysonin <ivysonthauan4@gmail.com>
2025-09-30 16:48:23 -0300

    Maneiras de definir Tuplas

---
+++ Lógica_de_programação/estrutura_de_dados/tuplas.py
@@ -0,0 +1,10 @@
+# Tupla com múltiplos elementos
+cores = ("Vermelho", "Azul", "Amarelo")
+
+# Tupla vazia
+cadernos = tuple()
+lapis = ()
+
+# Tupla com um único elemento (atenção ao uso da vírgula!)
+caneta = ("Azul",)  # Correto
+borracha = ("Branca")  # Incorreto: isso é uma string, não uma tupla
