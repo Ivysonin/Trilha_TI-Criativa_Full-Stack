c2861ce0778772d9962964f8216d134626159810
Ivysonin <ivysonthauan4@gmail.com>
2025-10-23 16:53:10 -0300

    Tipos de dados em JS

---
+++ JS/tipos_dados.js
@@ -0,0 +1,15 @@
+// String -> texto
+let saudacao = 'Olá, mundo!';
+console.log(saudacao);
+
+// Number -> 1, 2.2
+let idade = 18;
+console.log("Essa é minha idade:", idade);
+
+// Boolean -> verdadeiro ou falso
+let logado = true;
+console.log("Está logado:", logado);
+
+// Null -> valor nuol (intencionalmente vazio)
+let resposta = null;
+console.log(resposta);
