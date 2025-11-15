fd4c37fb69ee54d8a2d65ec176f2624a93e448ba
Ivysonin <ivysonthauan4@gmail.com>
2025-11-15 08:50:25 -0300

    Novos exercicios de POO

---
+++ JAVA/POO/exercicio_pessoas/Adulto.java
@@ -0,0 +1,6 @@
+public class Adulto extends Pessoa {
+    @Override
+    public void falar() {
+        System.out.println("Adulto falando!");
+    }
+}
---
+++ JAVA/POO/exercicio_pessoas/Crianca.java
@@ -0,0 +1,6 @@
+public class Crianca extends Pessoa{
+    @Override
+    public void falar() {
+        System.out.println("Criança falando!");
+    }
+}
